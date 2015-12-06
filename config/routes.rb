02a3a44d37d45594a9f954c384c2fb81ddf2e0ad
John Drago <jdrago.999@gmail.com>
2015-12-05 21:42:31 -0800

    Defined routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,45 @@ Rails.application.routes.draw do
     post 'auth' => 'auth#auth'
 
     scope ':vhost' do
+      post '' => 'vhost#create'
+      get '' => 'vhost#list'
+      scope ':id', constrants: lambda {|req| req.params['id'] =~ /^\d+$/ } do
+        get 'vhost#show'
+        put 'vhost#update'
+        delete 'vhost#delete'
+      end
       scope 'exchanges' do
+        post '' => 'exchange#create'
+        get '' => 'exchange#list'
         scope ':name' do
+          get '' => 'exchange#show'
+          delete '' => 'exchange#delete'
           scope 'bindings' do
+            get '' => 'binding#list_by_exchange'
           end
         end
       end
       scope 'queues' do
+        get '' => 'queue#list'
+        post '' => 'queue#create'
         scope ':name' do
+          get '' => 'queue#show'
+          delete '' => 'queue#delete'
           scope 'bindings' do
+            get 'binding#list_by_queue'
+            post 'binding#create'
             scope ':id' do
+              get 'binding#show'
+              delete 'binding#delete'
             end
           end
           scope 'messages' do
+            post '' => 'message#create'
+            scope ':id' do
+              get '' => 'message#show'
+              put '' => 'message#update'
+              delete '' => 'message#delete'
+            end
           end
         end
       end
